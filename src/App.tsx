ebbc4a37512a998db54d21abf716621a9399f83d
Ivan <i85chiosa@gmail.com>
2023-12-05 11:37:48 +0100

    Übung type Student

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    type NumericGrade = 0 | 1 | 2 | 3 | 4 | 5;
+    type LetterGrade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+
+
+    function outputStudent(student: Student): void {
+        console.log('='.repeat(30));
+        console.log(`${student.firstName} ${student.lastName} ${student.age}`);
+        console.log('='.repeat(30));
+        console.log('Grades:', student.grades);
+    }
+
+    const exampleStudent: Student = {
+        firstName: 'Ivan',
+        lastName: 'Chiosa',
+        age: 35,
+        grades: [1, 4, 3, 1, 3, 2, 1]
+    };
+    outputStudent(exampleStudent);
+
+
+    interface Student1 {
+        firstName: string;
+        lastName: string;
+        age: number;
+        grades: (NumericGrade | LetterGrade | undefined)[];
+    }
+
+    function outputStudent1(student: Student1): void {
+        console.log('='.repeat(30));
+        console.log(`${student.firstName} ${student.lastName} (${student.age})`);
+        console.log('='.repeat(30));
+        console.log('Noten:', student.grades.map(grade => (grade === undefined ? '*' : grade)));
+    }
+
+    const exampleStudent1: Student1 = {
+        firstName: 'Ivan',
+        lastName: 'Chiosa',
+        age: 35,
+        grades: ['A', 2, "F", 3, 1, "B", 2, 5, undefined],
+    };
+    outputStudent1(exampleStudent1);
+
+
+    function printAllStudents(students: Student1[]): void {
+        students.forEach(student => outputStudent1(student));
+    }
+
+    const studentList : Student1[] = [
+        {
+            firstName: 'Ivan',
+            lastName: 'Chiosa',
+            age: 35,
+            grades: ['A', 2, "F", 3, 1, "B", 2, 5, undefined],
+        },
+        {
+            firstName: 'Mario',
+            lastName: 'Rossi',
+            age: 35,
+            grades: ['A', 2, "F", 3, 1, "B", 2, 5, undefined],
+        },
+        {
+            firstName: 'Natali',
+            lastName: 'Dora',
+            age: 35,
+            grades: ['A', 2, "F", 3, 1, "B", 2, 5, undefined],
+        }
+    ]
+    printAllStudents(studentList);
+
+
+    return (
+        <>
+
+
+        </>
+    )
 }
 
 export default App
---
+++ src/Student.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    grades: number[];
+}
